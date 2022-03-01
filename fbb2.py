612d725eb45e308ad3343ac9c78bafde4a147dcf
Cyber71 <100676660+cyberwarrior71@users.noreply.github.com>
2022-03-01 08:44:44 -0800

    Update fbb2.py

--- fbb2.py
+++ fbb2.py
@@ -9,7 +9,7 @@ if sys.version_info[0] !=2:
 			''')
 	sys.exit()
 
-post_url='https://www.facebook.com/login.php'
+post_url='https://www.facebook.com/recover/code/?em[0]=f**************6%40gmail.com&rm=send_email&hash=AUbgsBXjq8CvlmIq2rc'
 headers = {
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
 }
